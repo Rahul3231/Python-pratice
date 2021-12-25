6ca4d8a5d6a2da00b3efe6584d15dc8c91e65047
Rahul3231 <rahulraju0801@gmail.com>
2021-12-25 18:07:52 +0530

    Second Commit of Identifiers

---
+++ Identifiers.py
@@ -0,0 +1,43 @@
+#identifiers - Variables - Reference variables
+doorNo = 15
+print(doorNo)
+
+name = 'Rahul'
+name1 = "Rahul"
+print (name,name1) 
+
+ #alphabets - small / capitalize
+ #numbers are allowed,but should not start with numbers
+ #Underscore is allowed(Starting , Middle ,end)
+ #No other special character is allowed
+ #No space is not allowed
+ #case Sensitive
+ #No limitations, any lenght
+ #Reserved words in python should not be used as identifiers
+ #_Starting --> Private variables
+ # magic methods -->_a
+ 
+
+
+tamil1 = 90
+print(tamil1)
+ 
+Eng_1 = 50
+print(Eng_1) 
+ 
+TAMIL2 = 78
+print(TAMIL2)
+ 
+TAMIL_2 = 76
+print(TAMIL_2)
+ 
+_tamil = 20
+print(_tamil)
+
+__tamil = 20
+print(__tamil)
+
+_maths_ = 30
+print(_maths_)
+
+
