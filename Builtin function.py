87dc46a6ba899ac054e086e178422262da986778
Rahul3231 <rahulraju0801@gmail.com>
2021-12-27 20:34:37 +0530

    Builtin Function - Fourth Commit

---
+++ Builtin function.py
@@ -0,0 +1,82 @@
+##Boolean
+no = 10
+no1 = 20
+print(no<no1)#10<20                         True
+print(no>no1)#10>20                         False
+print(no==no1)#comparsion equal 10==20      False
+print(no!=no1)#comparsion not equal 10!=20  True
+
+present = True
+print (present)
+print(type(present))
+
+print (True+True)
+print (True+False)
+print (False+False)
+
+######
+###### STR
+
+Email_ID ="""rahul's mail id is rahul@gmail.com he said "Hi" """
+print(Email_ID)
+
+Address ="""7/3 narayana maisti
+2nd st ,otteri
+chennai 600012"""
+print(Address)
+name = 'Rahul'
+City = 'chennai'
+print(name,City)
+print(name+City)#Str+Str
+
+Name = 'rahul'
+Pincode = 600012
+print(Name,Pincode)
+
+#String Slicing:
+#index - order
+#Forward / backward
+#Forward - +Ve -starts from zero
+#backward --Ve starts from -1
+
+name = 'rahul'
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+
+name = 'rajarajachozhn'
+     #  01234567 
+
+
+
+
+name = 'abcdefghijklmnopqrstuvwxyz'
+       # 
+print(len(name))
+print(name[-26:26])
+print(name[-26:-1:4])#step operator
+print(name[0:26])
+print(name[-26:-1])
+ 
+print(name[1:10:2])
+  
+print(name[8:1:-1])
+print(name[26::-1])
+
+  
+
+
+
+
+
+
+
+
