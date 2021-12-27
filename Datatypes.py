f9c092bc7d1a38cac596ffa4f487d13d98e7f2e7
Rahul3231 <rahulraju0801@gmail.com>
2021-12-27 20:28:26 +0530

    Third commit Datatypes

---
+++ Datatypes.py
@@ -0,0 +1,38 @@
+no = 10   
+print(no)
+print(type(no))
+
+#input - parameter / argument
+#action - Function / method / procedure /routine
+no = 4.5
+print(no)
+print(type(no))
+
+result = True
+print(type(result))
+
+name = "Rahul"
+print(len(name))
+
+no = 10
+print(no) #datatype
+print(id(no)) #address
+
+
+no = 123.45689123
+print(no) 
+print(round(no))
+print(round(no,2))
+
+no = 0o776
+print(no)
+
+
+
+
+
+
+
+
+
+
