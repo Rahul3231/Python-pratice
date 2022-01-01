4a6930c4bbf6f90eae9672828357e5809ff8b7dd
Rahul3231 <rahulraju0801@gmail.com>
2022-01-01 18:26:21 +0530

    Def_function _python

---
+++ Def_Func.py
@@ -0,0 +1,13 @@
+def greater(n1,n2):  #Function Definition name "greater"
+    if n1>n2:
+        print(n1, " is greater")
+    else:
+        print(n2, " is greater")
+
+#no1 =int(input("Enter no1:"))  #variables
+#no2 =int(input("Enter no2:"))
+
+#greater(no1,no2)  #Function calling Statement
+#arguments for given function
+
+
